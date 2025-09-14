3d73d66b4a45419101171375a3c6b7bf149848d7
MR-AMAN123 <mohdaman7054278971@gmail.com>
2025-09-14 14:38:53 +0530

    xdcf

--- client/src/pages/student/Course.jsx
+++ client/src/pages/student/Course.jsx
@@ -7,7 +7,7 @@ import { Link } from "react-router-dom";
 const Course = ({ course }) => {
   console.log(course)
   return (
-    <Link to={`/course-detail/${course._id || course.id || course.courseId}`}>
+    <Link to={`/course-detail/${course._id}`}>
       <Card className="overflow-hidden rounded-lg dark:bg-gray-800 bg-white shadow-lg hover:shadow-2xl transform hover:scale-105 transition-all duration-300">
         <div className="relative">
           <img
--- client/src/pages/student/Courses.jsx
+++ client/src/pages/student/Courses.jsx
@@ -19,7 +19,7 @@ const Courses = () => {
               ))
             : data?.courses &&
               data.courses.map((course, index) => (
-                <Course  key={course._id || course.id} course={course}/>
+                <Course  key={course._id || course.id} course={course?.data}/>
               ))}
              
         </div>
