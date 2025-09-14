2670a82c117cb0316d95c1788c40f658724a855a
MR-AMAN123 <mohdaman7054278971@gmail.com>
2025-09-14 16:13:41 +0530

    s

--- client/src/components/AnnouncementBar.jsx
+++ client/src/components/AnnouncementBar.jsx
@@ -19,13 +19,13 @@ const AnnouncementBar = () => {
   if (!visible) return null;
 
   return (
-    <div className="sticky top-16 w-full bg-gray-600 text-white text-sm px-4 py-2 flex item-center justify-between z-30 shadow-md">
-      <p className="font-medium ml-96">
+    <div className="sticky top-16 w-full bg-gray-600 text-white text-sm px-4 py-2 flex items-center justify-between z-30 shadow-md">
+      <p className="font-medium flex-1 text-center sm:text-left truncate">
         🎉 Welcome! Get 20% off on your first course. Learn from here to clear your concept from our best content
       </p>
       <button
         onClick={handleClose}
-        className="ml-4 p-1 rounded hover:bg-blue-700 transition"
+        className="ml-2 p-1 rounded hover:bg-blue-700 transition"
       >
         <X className="h-4 w-4" />
       </button>
